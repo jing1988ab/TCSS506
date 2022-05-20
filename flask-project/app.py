82a14facb87a8097a0520b9b04f11882e51f6063
yan <jing1988b@gmail.com>
2022-05-19 17:21:33 -0700

    hw5

--- flask-project/app.py
+++ flask-project/app.py
@@ -1,35 +1,31 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
-# from yelp import find_coffee
 from wiki import findBirths
 from flask_login import current_user, login_user, login_required, logout_user
 from models import db, login, UserModel
-# import rds_db as db
 
-DBUSER = 'postgres'
-DBPASS = 'Tcss_506_restaurant!'
-DBHOST = 'database-tcss506.c9xp7kbwvtsm.us-east-1.rds.amazonaws.com'
-DBPORT = '5432'
-DBNAME = 'pgliogindb'
 
 class loginForm(FlaskForm):
-    email=StringField(label="Enter email", validators=[DataRequired(),Email()])
-    password=PasswordField(label="Enter password",validators=[DataRequired(), Length(min=6,max=16)])
+    username=StringField(label="Enter username",validators=[DataRequired(), Length(min=6,max=20)])
+    password=PasswordField(label="Enter password",validators=[DataRequired(), Length(min=8)])
     submit=SubmitField(label="Login")
 
 class registerForm(FlaskForm):
+    username=StringField(label="Enter username",validators=[DataRequired(), Length(min=6,max=20)])
     email=StringField(label="Enter email", validators=[DataRequired(),Email()])
-    password=PasswordField(label="Enter password",validators=[DataRequired(), Length(min=6,max=16)])
+    password=PasswordField(label="Enter password",validators=[DataRequired(), Length(min=8)])
     submit=SubmitField(label="Register")
 
-#passwords={}
-#passwords['lhhung@uw.edu']='qwerty'
+DBUSER= 'lhhung'
+DBPASS= 'password'
+DBHOST= 'db'
+DBPORT= '5432'
+DBNAME= 'pglogindb'
 
 app = Flask(__name__)
 app.secret_key="a secret"
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/login.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}'.format(
     user=DBUSER,
     passwd=DBPASS,
@@ -42,11 +38,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 login.init_app(app)
 
-def addUser(email, password):
+def addUser(email, username, password):
     #check if email or username exits
     user=UserModel()
     user.set_password(password)
     user.email=email
+    user.username=username
     db.session.add(user)
     db.session.commit()
 
@@ -56,7 +53,7 @@ def create_table():
     db.create_all()
     user = UserModel.query.filter_by(email = "lhhung@uw.edu").first()
     if user is None:
-        addUser("lhhung@uw.edu","qwerty")    
+        addUser("lhhung@uw.edu","lhhung", "qwerty")    
     
 @app.route("/home",methods=['GET','POST'])
 @login_required
@@ -69,6 +66,9 @@ def findBirthday():
         monthinput = form_value_array[1]+'/'+form_value_array[2]
         yearinput = form_value_array[0]
         sizeinput=request.form["size"]
+        if int(sizeinput)< 1 or int(sizeinput) > 20:
+            print("herer")
+            return render_template("home.html")
         if monthinput and yearinput and sizeinput:
             return render_template("home.html", myData=findBirths(monthinput,yearinput,sizeinput))
     return render_template("home.html")
@@ -79,18 +79,19 @@ def redirectToLogin():
 
 @app.route("/login",methods=['GET','POST'])
 def login():
+    msg = ""
     form=loginForm()
     if form.validate_on_submit():
         if request.method == "POST":
-            email=request.form["email"]
+            username=request.form["username"]
             pw=request.form["password"]
-            user = UserModel.query.filter_by(email = email).first()
+            user = UserModel.query.filter_by(username = username).first()
             if user is not None and user.check_password(pw) :
                 login_user(user)
                 return redirect('/home')
             else:
-                return redirect('/register')
-    return render_template("login.html",form=form)
+                msg = "Please enter correct login details or register"
+    return render_template("login.html",form=form,msg=msg)
 
 @app.route('/logout')
 def logout():
@@ -107,25 +108,21 @@ def register():
     if form.validate_on_submit():
         if request.method == "POST":
             email=request.form["email"]
-            print(email)
+            username=request.form["username"]
             pw=request.form["password"]
             user = UserModel.query.filter_by(email = email).first()
             print(user)
-            if user is not None and user.check_password(pw) :
-                login_user(user)
-                return redirect('/home')
-            elif user is not None and not user.check_password(pw) :
+            if user is not None:
+                flash("User already exist. Please login!")
                 return redirect('/login')
+            # elif user is not None and not user.check_password(pw) :
+            #     return redirect('/login')
             else:
                 pw = str(pw)
-                addUser(email, pw)
+                addUser(email, username, pw)
+                flash("Register success. Please login!")
                 return redirect('/login')
-            # db.insert_details(email,pw)
-            # details = db.get_details()
-            # print(details)
-            # for detail in details:
-            #     var = detail
-            # return render_template("index.html", var = var)
+            
     return render_template("register.html",form=form)
 
 if __name__ == "__main__":
